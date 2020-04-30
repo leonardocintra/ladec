4a2a9f703c82c6805e149fedecaaa3f791860c99
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-04-30 12:04:28 -0300

    preparando para heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ladec.wsgi --log-file -
--- ladec/settings.py
+++ ladec/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,6 +84,9 @@ DATABASES = {
     }
 }
 
+# Update database configuration with $DATABASE_URL.
+DATABASES['default'].update(dj_database_url.config())
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -121,4 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
