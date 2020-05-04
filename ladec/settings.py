711ed67bd6a77ead0db70d072a19fd1015ff518e
Leonardo Cintra <leonardo.cintra@luizalabs.com>
2020-05-04 09:01:32 -0300

    add cloudinary

--- ladec/settings.py
+++ ladec/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
